661358ec2715547162061503e471f69f75355a9e
Ana Liebert <analiebert19@gmail.com>
2022-03-08 19:09:30 -0500

    heroku edits

--- mixtape_backend/settings.py
+++ mixtape_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,4 +133,4 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
