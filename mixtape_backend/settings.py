c68c14363dfe273977efa8d1b346b0f7028b1ff7
Ana Liebert <analiebert19@gmail.com>
2022-03-11 17:11:16 -0500

    heroku edit

--- mixtape_backend/settings.py
+++ mixtape_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5+&ce6%aj#cj@!e_2*bm61+uol0o3jdyn(u&%6jo@1-mv8#9q('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
