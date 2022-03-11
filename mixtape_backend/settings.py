43847c188a6b770036f458cbb2b89f9bbad08553
Ana Liebert <analiebert19@gmail.com>
2022-03-11 16:23:49 -0500

    app edit

--- mixtape_backend/settings.py
+++ mixtape_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +140,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+django_heroku.settings(locals())
