f61eb57b2f09e1f4abfa672d2b9bf51054907761
Ana Liebert <analiebert19@gmail.com>
2022-03-13 18:22:59 -0400

    view changes for mix

--- api/serializers.py
+++ api/serializers.py
@@ -1,40 +1,6 @@
 from .models import Host, Mix, Genre, UserProfile
-# User
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-# from django.contrib.auth import get_user_model
-
-# User = get_user_model()
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         exclude = ('password', )
-
-
-# class RegistrationSerializer(serializers.ModelSerializer):
-#     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
-
-#     class Meta:
-#         model = User
-#         fields = ['email', 'username', 'password', 'password2']
-#         extra_kwargs = {
-#             'password': {'write_only': True}
-#         }
-#     # override the save method to validate
-#     def save(self):
-#         user = User(
-#             email=self.validated_data['email'],
-#             username=self.validated_data['username'],
-#         )
-#         password = self.validated_data['password']
-#         password2 = self.validated_data['password2']
-
-#         if password != password2:
-#             raise serializers.ValidationError({'password': 'Passwords must match.'})
-#         user.set_password(password)
-#         user.save()
-#         return user
 
 
 
@@ -48,6 +14,14 @@ class HostSerializer(serializers.ModelSerializer):
 
 class MixSerializer(serializers.ModelSerializer):
     genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), many=True)
+    # For many-to-many relationships set() accepts a list of either model instances or field values, normally primary keys, as the objs argument.
+    # genre = serializers.SlugRelatedField(
+    #     many=True,
+    #     read_only=False,
+    #     slug_field='genre_name',
+    #     queryset=Genre.objects.all()
+    # )
+
     class Meta:
         model = Mix
         fields = ['id' ,'title', 'description', 'created_at', 'host', 'genre', 'image', 'soundcloudplayer', 'creator']
--- api/views.py
+++ api/views.py
@@ -5,7 +5,6 @@ from .models import Host, Mix, Genre, UserProfile
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import MixSerializer, HostSerializer, GenreSerializer, UserProfileSerializer
-# RegistrationSerializer, UserSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.views import APIView
@@ -13,12 +12,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 
-# class MeView(APIView):
-#     permission_classes = [IsAuthenticated]
 
-#     def get(self, request):
-#         serializer = UserSerializer(request.user)
-#         return Response(serializer.data)
 
 class HostViewSet(viewsets.ModelViewSet):  
     queryset = Host.objects.all()  
@@ -29,23 +23,11 @@ class HostViewSet(viewsets.ModelViewSet):
 class MixViewSet(viewsets.ModelViewSet):
     queryset = Mix.objects.all().order_by('created_at')
     serializer_class = MixSerializer
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    # def get_queryset(self):
-    #     qs = super().get_queryset()
 
-    #     # Get only contact about current authenticated user
-    #     qs = qs.filter(user=self.request.user)
+    def create(self, request, *args, **kwargs):
+    print request.data
+    return super(MixViewSet, self).create(request, *args, **kwargs)
 
-    #     # Add search capabilities
-    #     search = self.request.query_params.get("search", None)
-    #     if search:
-    #         qs = qs.filter(
-    #             models.Q(name__icontains=search)
-    #             | models.Q(phone__icontains=search)
-    #             | models.Q(email__icontains=search)
-    #         )
-
-    #     return qs
 
 class GenreViewSet(viewsets.ModelViewSet):  
     queryset = Genre.objects.all()
@@ -56,6 +38,3 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
-# class UserView(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = RegistrationSerializer
