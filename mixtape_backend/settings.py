b149c29e5e32612bd9ff9bb74d59b248b9e58b5d
Ana Liebert <analiebert19@gmail.com>
2022-03-17 18:49:09 -0400

    cors whitelist

--- mixtape_backend/settings.py
+++ mixtape_backend/settings.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
+    'https://inspiring-lamport-ed6060.netlify.app'
 ]
 
 REST_FRAMEWORK = {
